b8fa0df521493494e4f40d26425ae726495eace1
Danny Nguyen <nguyencaodat27052004@gmail.com>
2023-07-11 21:27:11 -0600

    Adjust the Menu Page

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -11,157 +11,98 @@ import Pepsi from "../../Assets/Images/pepsi.png";
 import BurgerAndFries from "../../Assets/Images/burgerAndFries.png";
 import Burger from "../../Assets/Images/burger.png";
 
+const MenuItem = ({ category, name, desc, price, image }) => {
+  return (
+    <div>
+      {category && (
+        <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest'>
+          {category}
+        </div>
+      )}
+      <div className='row text-white'>
+        {image && <img src={image} alt={name} className='w-full h-full object-cover mx-auto my-7' />}
+        <div className=" flex items-center justify-between">
+          <div className='left'>
+            <h3 className='text-banana font-bold text-xl mb-2'>{name}</h3>
+            <p className="text-grey">{desc}</p>
+          </div>
+          <div className='right'>
+            <span>${price.toFixed(2)}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const getCategoryImage = (category) => {
+  switch (category) {
+    case "SOUP & SALAD":
+      return Burger;
+    case "HALF & HALF":
+      return Sandwich;
+    case "BEVERAGES":
+      return Pepsi;
+    case "SANDWICHES":
+      return BurgerAndFries;
+    case "ENTRÉE":
+      return Entree;
+    default:
+      return null;
+  }
+};
+
 const Menu = () => {
   return (
     <div className='menu-wrapper'>
       <div className='menu-container'>
         <Header />
         <div className='image-container innerWidth h-[75vh]'>
-          <img
-            src={MenuImage}
-            alt='menu image'
-            className='w-full h-full object-cover'
-          />
-
+          <img src={MenuImage} alt='menu image' className='w-full h-full object-cover' />
           <span className='text-white text-[6rem]'>Menu</span>
         </div>
-
         <div className='menuList-container px-[9rem] py-[3rem] bg-black innerWidth flex flex-col items-center justify-center'>
           <img src={FoodPic} alt='food Picture' width={800} />
-
           <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest mt-[3rem]'>
-            * APPETIZERS *
+            APPETIZERS
           </div>
           <div>
-            {MenuList.food.map((item, i) => {
-              if (item.name === "Some of the Day") {
-                return (
-                  <div key={i}>
-                    <img src={Burger} alt='Burger' width={800} className='mx-auto my-7'/>
-                    <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest'>
-                      * SOUP & SALAD *
-                    </div>
-                    <div className='row text-white flex items-center justify-between'>
-                      <div className='left'>
-                        <h3 className='text-banana font-bold text-xl'>
-                          {item.name}
-                        </h3>
-                        <p>{item.desc}</p>
-                      </div>
-                      <div className='right'>
-                        <span>${item.price.toFixed(2)}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (item.name === "Soup & Salad") {
-                return (
-                  <div key={i}>
-                    <img src={Sandwich} alt='Sandwich' width={800} className='mx-auto my-7'/>
-                    <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest'>
-                      * HALF & HALF *
-                    </div>
-                    <div className='row text-white flex items-center justify-between'>
-                      <div className='left'>
-                        <h3 className='text-banana font-bold text-xl'>
-                          {item.name}
-                        </h3>
-                        <p>{item.desc}</p>
-                      </div>
-                      <div className='right'>
-                        <span>${item.price.toFixed(2)}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (item.name === "Hot Tea or Coffee") {
-                return (
-                  <div key={i}>
-                    <img src={Pepsi} alt='Pepsi' width={800} className='mx-auto my-7'/>
-                    <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest'>
-                      * BEVERAGES *
-                    </div>
-                    <div className='row text-white flex items-center justify-between'>
-                      <div className='left'>
-                        <h3 className='text-banana font-bold text-xl'>
-                          {item.name}
-                        </h3>
-                        <p>{item.desc}</p>
-                      </div>
-                      <div className='right'>
-                        <span>${item.price.toFixed(2)}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (item.name === "Irish BLT") {
-                return (
-                  <div key={i}>
-                    <img src={BurgerAndFries} alt='BurgerAndFries' width={800} className='mx-auto my-7'/>
-                    <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest'>
-                      * SANDWICHES *
-                    </div>
-                    <p className='text-blue'>
-                      Served with coleslaw Add fries, mashed potatoes or a side
-                      of baked beans - $3.50 Add sweet potato fries, onion
-                      rings, cup of soup or side salad - $4.0.0
-                    </p>
-                    <div className='row text-white flex items-center justify-between'>
-                      <div className='left'>
-                        <h3 className='text-banana font-bold text-xl'>
-                          {item.name}
-                        </h3>
-                        <p>{item.desc}</p>
-                      </div>
-                      <div className='right'>
-                        <span>${item.price.toFixed(2)}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (item.name === "Shepherd's Pie") {
-                return (
-                  <div key={i}>
-                    <img src={Entree} alt='Entree' width={800} className='mx-auto my-7'/>
-                    <div className='flex justify-center items-center text-[#E89314] font-extrabold text-xl tracking-widest'>
-                      * ENTRÉE *
-                    </div>
-                    <div className='row text-white flex items-center justify-between gap-4 flex-wrap'>
-                      <div className='left'>
-                        <h3 className='text-banana font-bold text-xl mb-6'>
-                          {item.name}
-                        </h3>
-                        <p className="text-grey">{item.desc}</p>
-                      </div>
-                      <div className='right'>
-                        <span>${item.price}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else {
-                return (
-                  <div key={i}>
-                    <div className='row text-white flex items-center justify-between'>
-                      <div className='left'>
-                        <h3 className='text-banana font-bold text-xl'>
-                          {item.name}
-                        </h3>
-
-                        <p>{item.desc}</p>
-                      </div>
-
-                      <div className='right'>
-                        <span>${item.price.toFixed(2)}</span>
-                      </div>
-                    </div>
-                  </div>
-                );
-              }
-            })}
+            {MenuList.food.map((item, i) => (
+              <MenuItem
+                key={i}
+                category={
+                  item.name === "Some of the Day"
+                    ? "SOUP & SALAD"
+                    : item.name === "Soup & Salad"
+                    ? "HALF & HALF"
+                    : item.name === "Hot Tea or Coffee"
+                    ? "BEVERAGES"
+                    : item.name === "Irish BLT"
+                    ? "SANDWICHES"
+                    : item.name === "Shepherd's Pie"
+                    ? "ENTRÉE"
+                    : ""
+                }
+                name={item.name}
+                desc={item.desc}
+                price={item.price}
+                image={getCategoryImage(
+                  item.name === "Some of the Day"
+                    ? "SOUP & SALAD"
+                    : item.name === "Soup & Salad"
+                    ? "HALF & HALF"
+                    : item.name === "Hot Tea or Coffee"
+                    ? "BEVERAGES"
+                    : item.name === "Irish BLT"
+                    ? "SANDWICHES"
+                    : item.name === "Shepherd's Pie"
+                    ? "ENTRÉE"
+                    : ""
+                )}
+              />
+            ))}
           </div>
         </div>
-
         <Footer />
       </div>
     </div>
