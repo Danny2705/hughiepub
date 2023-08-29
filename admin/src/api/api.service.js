ce4ea16e49b9f6faf2c99bccb0d232c38d0ddf8a
Danny Nguyen <nguyencaodat27052004@gmail.com>
2023-08-28 23:08:48 -0600

    update frontend

--- admin/src/api/api.service.js
+++ admin/src/api/api.service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api"
+  baseURL: process.env.API_URL || "https://hughie-api-vercel.vercel.app/api",
 });
 
 export const signUp = async (user) => {
